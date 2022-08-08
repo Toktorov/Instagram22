ad3dbc5a9b892055991e95372f52d016d4d4e0c6
Toktorov <ktoktorov2005@gmail.com>
2022-08-08 18:41:26 +0600

    CRUD system for post

--- posts/views.py
+++ posts/views.py
@@ -24,3 +24,31 @@ def post_create(request):
         post = Post.objects.create(user = request.user, title = title, description = description, image = image)
         return redirect('index')
     return render(request, 'post_create.html')
+
+def post_update(request, id):
+    post = Post.objects.get(id = id)
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        image = request.FILES   .get('image')
+        post = Post.objects.get(id = id)
+        post.title = title
+        post.description = description
+        post.image = image 
+        post.save()
+        return redirect('post_detail', post.id)
+    context = {
+        'post' : post,
+    }
+    return render(request, 'post_update.html', context)
+
+def post_delete(request, id):
+    post = Post.objects.get(id = id)
+    if request.method == "POST":
+        post = Post.objects.get(id = id)
+        post.delete()
+        return redirect('index')
+    context = {
+        'post' : post,
+    }
+    return render(request, 'post_delete.html', context)
